5ceeb0d199238f101dd0d61124a19799d34e73c4
German Meza <iam@germanamz.com>
2026-04-02 17:58:58 -0600

    test(e2e): add TestMain that builds tusk binary once for all e2e tests

---
+++ tests/e2e/main_test.go
@@ -0,0 +1,43 @@
+// tests/e2e/main_test.go
+package e2e
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+// binPath is the path to the compiled tusk binary, set in TestMain.
+var binPath string
+
+func TestMain(m *testing.M) {
+	// Build the tusk binary into a temp directory.
+	tmpDir, err := os.MkdirTemp("", "tusk-e2e-bin-*")
+	if err != nil {
+		panic("creating temp dir for binary: " + err.Error())
+	}
+	defer os.RemoveAll(tmpDir)
+
+	binPath = filepath.Join(tmpDir, "tusk")
+	cmd := exec.Command("go", "build", "-o", binPath, "./cmd/tusk")
+	// Build from the project root. The test runs from tests/e2e/, so go up two levels.
+	cmd.Dir = filepath.Join(mustGetwd(), "..", "..")
+	cmd.Env = append(os.Environ(), "CGO_ENABLED=1")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		panic("building tusk binary: " + err.Error())
+	}
+
+	os.Exit(m.Run())
+}
+
+func mustGetwd() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic("getwd: " + err.Error())
+	}
+	return wd
+}
