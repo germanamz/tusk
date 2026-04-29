054841f67bb3d9ec55629443606a386712d45dd7
German Meza <iam@germanamz.com>
2026-04-29 12:41:25 +0000

    feat(v0.14): add blankline analyzer (rule 2)
    Implements internal/lint/blankline with analyzer.go, analyzer_test.go,
    and testdata/src/a/a.go. The analyzer detects missing blank lines before
    and after if-err (and <noun>Err) guards in every function body.

---
+++ internal/lint/blankline/analyzer_test.go
@@ -0,0 +1,13 @@
+package blankline_test
+
+import (
+	"testing"
+
+	"github.com/germanamz/tusk/internal/lint/blankline"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func TestBlankline(test *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(test, testdata, blankline.Analyzer, "a")
+}
